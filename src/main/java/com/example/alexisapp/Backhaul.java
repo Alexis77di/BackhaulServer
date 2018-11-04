8bab8e4f6362cc9fb767b2e6f8419ad1d9de3cfd
Alexis77di <alexischai197@gmail.com>
2018-11-04 23:06:09 +0200

    (Hibernate + Table) Commit- Backhaul Server

--- src/main/java/com/example/alexisapp/Backhaul.java
+++ src/main/java/com/example/alexisapp/Backhaul.java
@@ -1,8 +1,53 @@
 package com.example.alexisapp;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+
 public class Backhaul {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        Configuration cfg = new Configuration();
+        Properties p = new Properties();
+//load properties file
+        try {
+            p.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("hibernate.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        cfg.setProperties(p);
+        cfg.addAnnotatedClass(BackhaulData.class);
+// build session factory
+
+
+        try (SessionFactory sessionFactory = cfg.buildSessionFactory()) {
+            persist(sessionFactory);
+            load(sessionFactory);
+        }
+    }
+
+    private static void load(SessionFactory sessionFactory) {
+        System.out.println("-- loading persons --");
+        Session session = sessionFactory.openSession();
+        List<BackhaulData> persons = session.createQuery("FROM BackhaulData", BackhaulData.class).list();
+        persons.forEach(System.out::println);
+        session.close();
+    }
+
+    private static void persist(SessionFactory sessionFactory) {
+        BackhaulData p1 = new BackhaulData();
+        BackhaulData p2 = new BackhaulData();
+        System.out.println(p1);
+        System.out.println(p2);
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(p1);
+        session.save(p2);
+        session.getTransaction().commit();
     }
 }
-
---
+++ src/main/java/com/example/alexisapp/BackhaulData.java
@@ -0,0 +1,73 @@
+package com.example.alexisapp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.StringJoiner;
+
+@Entity
+public class BackhaulData {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String address;
+    private String gps_signal;
+    private int criticallity_level;
+    private LocalDateTime timestamp;
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", BackhaulData.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("address='" + address + "'")
+                .add("gps_signal='" + gps_signal + "'")
+                .add("criticallity_level=" + criticallity_level)
+                .add("timestamp=" + timestamp)
+                .toString();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGps_signal() {
+        return gps_signal;
+    }
+
+    public void setGps_signal(String gps_signal) {
+        this.gps_signal = gps_signal;
+    }
+
+    public int getCriticallity_level() {
+        return criticallity_level;
+    }
+
+    public void setCriticallity_level(int criticallity_level) {
+        this.criticallity_level = criticallity_level;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+}
+
