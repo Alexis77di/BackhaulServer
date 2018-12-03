f231c4629fe07f5a9902d8bd028d2f9857306840
Alexey77 <sdi1300197@di.uoa.gr>
2018-12-03 16:03:20 +0200

    WebSocket Client

--- src/main/java/com/example/alexisapp/MessageEncoder.java
+++ src/main/java/com/example/alexisapp/MessageEncoder.java
@@ -1,27 +1,27 @@
-package com.example.alexisapp;
-
-import com.google.gson.Gson;
-
-import javax.websocket.Encoder;
-import javax.websocket.EndpointConfig;
-
-public class MessageEncoder implements Encoder.Text<Message> {
-
-    private static Gson gson = new Gson();
-
-    @Override
-    public String encode(Message message) {
-        String json = gson.toJson(message);
-        return json;
-    }
-
-    @Override
-    public void init(EndpointConfig endpointConfig) {
-        // Custom initialization logic
-    }
-
-    @Override
-    public void destroy() {
-        // Close resources
-    }
-}
+//package com.example.alexisapp;
+//
+//import com.google.gson.Gson;
+//
+//import javax.websocket.Encoder;
+//import javax.websocket.EndpointConfig;
+//
+//public class MessageEncoder implements Encoder.Text<Message> {
+//
+//    private static Gson gson = new Gson();
+//
+//    @Override
+//    public String encode(Message message) {
+//        String json = gson.toJson(message);
+//        return json;
+//    }
+//
+//    @Override
+//    public void init(EndpointConfig endpointConfig) {
+//        // Custom initialization logic
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        // Close resources
+//    }
+//}
